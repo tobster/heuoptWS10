57bbf46d3246aecf0b678c27b942efae91ba9ace
Lukas Lang <lukaslang@lukaslang.(none)>
2011-01-06 23:51:55 +0100

    Reviewed code updated.

--- src/main/scala/at/ac/tuwien/opt/file/InstanceReader.scala
+++ src/main/scala/at/ac/tuwien/opt/file/InstanceReader.scala
@@ -7,11 +7,9 @@ object InstanceReader {
 
   private val p = Pattern.compile("""([0-9]*\.?[0-9]*)\s+([0-9]*\.?[0-9]*)""")
 
-  def readInstance(filename: String): Iterator[(Double, Double)] = for {
-    line <- Source.fromFile(filename).getLines
+  def readInstance(filename: String): Iterator[(Double, Double)] = for (
+    line <- Source.fromFile(filename).getLines;
     val m = p.matcher(line)
     if m.find
-    val a = m.group(1).toDouble
-    val b = m.group(2).toDouble
-  } yield (a, b)
+  ) yield (m.group(1).toDouble, m.group(2).toDouble);
 }

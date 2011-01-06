36a5040e34402eac028cec107ef3bff18b2d6da6
Tobias Hochwallner <tobster@gmx.at>
2011-01-06 22:51:43 +0100

    curyied DistanceHelper

--- src/test/scala/at/ac/tuwien/util/DistanceHelper.scala
+++ src/test/scala/at/ac/tuwien/util/DistanceHelper.scala
@@ -3,7 +3,7 @@ package at.ac.tuwien.util
 import scala.math._
 
 trait DistanceHelper {
-  def distance(a: (Double, Double), b: (Double, Double)): Double = {
-    return pow(pow(a._1 - b._1, 2) + pow(a._2 - b._2, 2), 1.5)
+  def distance(a: Double, b: Double)(c: Double, d: Double): Double = {
+    return pow(pow(a - c, 2) + pow(b - d, 2), 1.5)
   }
 }

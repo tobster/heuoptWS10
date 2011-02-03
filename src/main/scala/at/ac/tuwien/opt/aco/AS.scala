4c0e028cbc7822d1a2fe9357f58226bc0846b95f
Lukas Lang <lukas.lang@inode.at>
2011-02-03 11:23:33 +0100

    Added nodes by distance.

--- src/main/scala/at/ac/tuwien/opt/aco/AS.scala
+++ src/main/scala/at/ac/tuwien/opt/aco/AS.scala
@@ -3,51 +3,67 @@ package at.ac.tuwien.opt.aco
 import scala.math._
 import at.ac.tuwien.opt.util.DistanceHelper
 import scala.util.Random
+import collection.immutable.SortedSet
 
-/**
- * Created by IntelliJ IDEA.
- * User: tobi
- * Date: 16.01.11
- * Time: 22:57
- * To change this template use File | Settings | File Templates.
- */
-
-class AS(val instance: List[(Double, Double)],
-         alpha: Double = .5,
-         beta: Double = .5,
-         initialPheromone: Double = 1.0)
+class Ant(instance: List[(Double, Double)],
+          alpha: Double = .5,
+          beta: Double = .5,
+          initialPheromone: Double = 1.0)
   extends DistanceHelper {
-  type Matrix = Array[Array[Double]]
+
+  def recChoose(source: Int, xs: List[Int], prob: Double, border: Double): Option[Int] = {
+    val tmp: Double = prob + p(source, xs.head)
+    xs match {
+      case Nil => None
+      case x :: xs if tmp >= border => Some(x)
+      case _ => recChoose(source, xs.tail, tmp, border)
+    }
+  }
+
+  def chooseTarget(source: Int): Option[Int] = {
+    recChoose(source, neighbourhood.toList, 0, Random.nextDouble)
+  }
+
+  def chooseSource(): Int = visited.toList(Random.nextInt(visited.size))
+
   //pheromones
-  val tau: Matrix = Array.fill(instance.size, instance.size) {
+  val tau: Array[Array[Double]] = Array.fill(instance.size, instance.size) {
     initialPheromone
   }
   //distances
-  val ny: Matrix = Array.tabulate(instance.size, instance.size)((i: Int, j: Int) => distance(instance(i), instance(j)))
-
-  def chooseTarget(source: Int): Option[Int] = {
-    val r = Random.nextDouble
-
-    def recChoose(xs: List[Int], prob: Double): Option[Int] = {
-      val tmp:Double = prob + p(source, xs.head)
-      xs match {
-        case Nil => None
-        case x :: xs if tmp >= r => Some(x)
-        case _ => recChoose(xs.tail, tmp)
-      }
-    }
-    recChoose(neighbourhood, 0)
-  }
-
-  def chooseSource(): Int = visited(Random.nextInt(visited.size))
-
+  val ny: Array[Array[Double]] = Array.tabulate(instance.size, instance.size)((i: Int, j: Int) => distance(instance(i), instance(j)))
   // unvisited neighbours.
-  val neighbourhood: List[Int] = List.range(1, instance.size)
+  val neighbourhood: Set[Int] = List.range(1, instance.size).toSet
   // the visited cities.
-  val visited: List[Int] = List(0)
+  val visited: Set[Int] = Set(0)
+
+  val nodesByDistance: Array[SortedSet[Int]] = Array.tabulate(instance.size)((i: Int) =>
+    SortedSet(Seq.range(0, instance.size))(new Ordering[Int] {
+        override def compare(x:Int, y:Int): Int = {
+           ny(i)(x).compare(ny(i)(y))
+        }
+    }))
 
   def p(i: Int, j: Int): Double = {
     pow(tau(i)(j), alpha) * pow(ny(i)(j), beta) /
       (0.0 /: neighbourhood)((total, l) => total + pow(tau(i)(l), alpha) * pow(ny(i)(l), beta))
   }
+
+  def reached(source: Int, target: Int): Set[Int] = {
+
+  }
+
+  def solve(iterations: Int): (Double, List[(Int, Int)]) = {
+    for (t <- 0 to iterations) {
+      while (!neighbourhood.isEmpty) {
+/*        val source = chooseSource()
+        val target = chooseTarget(source).get
+        val reached:Set[Int] = reached(source, target)
+        visited += reached
+        neighbourhood -= reached*/
+      }
+    }
+    return (.0, Nil)
+  }
+
 }
--- src/test/scala/at/ac/tuwien/opt/aco/ASTest.scala
+++ src/test/scala/at/ac/tuwien/opt/aco/ASTest.scala
@@ -4,16 +4,34 @@ import at.ac.tuwien.opt.file.InstanceReader
 import org.scalatest.matchers.MustMatchers
 import org.junit.Test
 import org.junit.Assert._
-import org.scalatest.junit.{JUnitSuite, AssertionsForJUnit}
-
+import org.scalatest.junit.JUnitSuite
+import org.hamcrest.core.Is._
 class ASTest extends JUnitSuite with MustMatchers {
 
-  var instance = InstanceReader.readInstance("target/scala_2.8.1/test-resources/mebp-02.dat")
-  var as = new AS(instance = instance.toList,alpha = 1,beta = 1)
 
   @Test def testP() {
+    var instance = InstanceReader.readInstance("target/scala_2.8.1/test-resources/mebp-02.dat")
+    var as = new Ant(instance = instance.toList, alpha = 1, beta = 1)
+    assertEquals(as.p(0, 1), .5, .0001)
+    println(as.p(0, 1))
+    println(as.p(0, 2))
+    assertEquals(as.p(0, 2), .5, .0001)
+    assertThat(as.recChoose(0, List(1, 2), 0.0, 0.3), is(Some(1).asInstanceOf[Option[Int]]))
+    assertThat(as.recChoose(0, List(1, 2), 0.0, 0.6), is(Some(2).asInstanceOf[Option[Int]]))
+    assertThat(as.recChoose(0, List(1, 2), 0.0, 0.5), is(Some(1).asInstanceOf[Option[Int]]))
+    assertThat(as.recChoose(0, List(2), 0.5, 0.6), is(Some(2).asInstanceOf[Option[Int]]))
+    //assertThat(as.recChoose(0, Nil, 0.5, 0.6), is(None.asInstanceOf[Option[Int]]))
 
 
+  }
+
+  @Test def nodesByDistanceTest() {
+    var instance = InstanceReader.readInstance("target/scala_2.8.1/test-resources/mebp-03.dat")
+    var as = new Ant(instance = instance.toList, alpha = 1, beta = 1)
+    assertThat(as.nodesByDistance(0).toList, is(List(0,1,2,3)));
+    assertThat(as.nodesByDistance(1).toList, is(List(1,0,2,3)));
+    assertThat(as.nodesByDistance(2).toList, is(List(2,3,0,1)));
+    assertThat(as.nodesByDistance(3).toList, is(List(3,2,0,1)));
 
   }
 

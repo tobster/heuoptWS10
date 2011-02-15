6a7ed64ae38fa2f37591f7108716ae1fb2b3072f
Tobias Hochwallner <tobster@gmx.at>
2011-02-15 23:38:00 +0100

    fixed wrong parser regex

--- src/main/scala/at/ac/tuwien/opt/file/InstanceReader.scala
+++ src/main/scala/at/ac/tuwien/opt/file/InstanceReader.scala
@@ -6,7 +6,7 @@ import at.ac.tuwien.opt.aco.Instance
 
 object InstanceReader {
 
-  private val p = Pattern.compile("""([0-9]*\.?[0-9]*)\s+([0-9]*\.?[0-9]*)""")
+  private val p = Pattern.compile("""([0-9]*\.[0-9]*)\s+([0-9]*\.?[0-9]*)""")
 
   def readInstance(filename: String): Instance = new Instance((for (
     line <- Source.fromFile(filename).getLines;
